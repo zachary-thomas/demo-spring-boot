56b36cca35e13da2d803889cb8a6c2a61be54d36
Zachary M Thomas <thomazac@mail.gvsu.edu>
2021-05-18 17:55:08 -0400

    starting demo

---
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Student;
+
+import java.util.List;
+
+public interface StudentService {
+
+    public List<Student> getStudents();
+}
---
+++ src/main/java/com/example/demo/service/StudentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Student;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Override
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+        );
+    }
+}
